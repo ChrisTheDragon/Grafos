5d4094d0f09140342f30b1227436477f710cd97a
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2023-05-01 19:30:08 -0300

    Pequenos Updates e comentarios

--- API Grafos 0.5.py
+++ API Grafos 0.5.py
@@ -1,3 +1,11 @@
+'''
+Trabalho 3 - Representação Computacional
+Descrição: Iniciando nossa API, implemente representações computaciona de Grafos por meio de Listas de Adjacências e Matrizes de Adjacências.
+
+Aluno: Christian J. C. Marinho
+Matricula: 202004940041
+'''
+
 class Grafos_Lista:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
@@ -5,14 +13,17 @@ class Grafos_Lista:
     
 
     def AddAresta(self, origem, destino):
+        # Verifica se a origem e o destino são válidos
         if destino > self.num_vertices or origem > self.num_vertices:
             print(f'ERRO: Aresta {origem, destino} fora do grafo\n')
         else:
+            # Adiciona a aresta na lista de adjacências de ambas as direções
             self.lista_adjacencias[origem-1].append(destino)
             self.lista_adjacencias[destino-1].append(origem)
         
     
     def mostrar_vertices(self):
+        # Percorre a lista de adjacências de cada vértice e imprime na tela
         for i in range(self.num_vertices):
             print(f"Vértice {i+1} -> ", end=" ")
             for j in self.lista_adjacencias[i]:
@@ -26,25 +37,27 @@ class Grafos_Lista:
 class Grafos_Matrizes:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
+        # Cria uma matriz de adjacências vazia com dimensão num_vertices x num_vertices
         self.matriz_adjacencias = [[0] * num_vertices for _ in range(num_vertices)]
 
 
     def AddAresta(self, origem, destino):
+        # Verifica se a origem e o destino são válidos
         if destino > self.num_vertices or origem > self.num_vertices:
             print(f'ERRO: Aresta {origem, destino} fora do grafo\n')
         else:
+            # Adiciona a aresta na lista de adjacências de ambas as direções
             self.matriz_adjacencias[origem-1][destino-1] = 1
             self.matriz_adjacencias[destino-1][origem-1] = 1
 
     def mostrar_vertices(self):
-        
+        # Percorre a matriz de adjacências e imprime na tela 1 para arestas e 0 para ausência de arestas
         print("    ", end="")
         for j in range(self.num_vertices):
             print(f"[{j+1}]", end="")
         print()
         
         for i in range(self.num_vertices):
-            #print(f"Vértice {i+1}:", end=" ")
             print(f'[{i+1}]', end=" ")
             for j in range(self.num_vertices):
                 if self.matriz_adjacencias[i][j] != 0:
--- API Grafos 1.0.py
+++ API Grafos 1.0.py
@@ -1,3 +1,10 @@
+'''
+Trabalho 4 - Métodos Básicos
+Descrição: Seguindo nossa API e a implementação computacional realizada na tarefa anterior, implemente uma classe Grafos que deve conter os sequintes métodos: a) V() retorna o número de vértices; b) A() retorna o número de arestas; c) AddArestas() - Adiciona arestas; Adj() retorna arestas adjacentes.
+
+Aluno: Christian J. C. Marinho
+Matricula: 202004940041
+'''
 
 class Grafos_Lista:
     def __init__(self, num_vertices):
@@ -19,14 +26,17 @@ class Grafos_Lista:
     
 
     def AddAresta(self, origem, destino):
+        # Verifica se a origem e o destino são válidos
         if destino > self.num_vertices or origem > self.num_vertices:
             print(f'ERRO: Aresta {origem, destino} fora do grafo\n')
         else:
+            # Adiciona a aresta na lista de adjacências de ambas as direções
             self.lista_adjacencias[origem-1].append(destino)
             self.lista_adjacencias[destino-1].append(origem)
         
     
     def mostrar_vertices(self):
+        # Percorre a lista de adjacências de cada vértice e imprime na tela
         for i in range(self.num_vertices):
             print(f"Vértice {i+1} -> ", end=" ")
             for j in self.lista_adjacencias[i]:
@@ -39,9 +49,13 @@ class Grafos_Lista:
         print(f'Adjacencias ao vertice {vertice}: {self.lista_adjacencias[vertice-1]}\n')
 
 
+
+
+
 class Grafos_Matrizes:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
+        # Cria uma matriz de adjacências vazia com dimensão num_vertices x num_vertices
         self.matriz_adjacencias = [[0] * num_vertices for _ in range(num_vertices)]
         
     
@@ -61,22 +75,23 @@ class Grafos_Matrizes:
 
 
     def AddAresta(self, origem, destino):
+        # Verifica se a origem e o destino são válidos
         if destino > self.num_vertices or origem > self.num_vertices:
             print(f'ERRO: Aresta {origem, destino} fora do grafo\n')
         else:
+            # Adiciona a aresta na lista de adjacências de ambas as direções
             self.matriz_adjacencias[origem-1][destino-1] = 1
             self.matriz_adjacencias[destino-1][origem-1] = 1
             
 
     def mostrar_vertices(self):
-        
+        # Percorre a matriz de adjacências e imprime na tela 1 para arestas e 0 para ausência de arestas
         print("    ", end="")
         for j in range(self.num_vertices):
             print(f"[{j+1}]", end="")
         print()
         
         for i in range(self.num_vertices):
-            #print(f"Vértice {i+1}:", end=" ")
             print(f'[{i+1}]', end=" ")
             for j in range(self.num_vertices):
                 if self.matriz_adjacencias[i][j] != 0:
